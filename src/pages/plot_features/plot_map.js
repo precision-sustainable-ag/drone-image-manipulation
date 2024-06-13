59564cb17edf026800f224b9a61d68af3bbdb062
Akshat Savla <akshat000822@gmail.com>
2024-06-13 11:49:14 -0400

    Added exporting plots as images

--- src/pages/plot_features/plot_map.js
+++ src/pages/plot_features/plot_map.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import {Button, Box, Grid, Typography} from '@mui/material';
 
@@ -8,6 +8,7 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import Feature from 'ol/Feature';
 import Projection from 'ol/proj/Projection';
+import { getBottomLeft, getTopRight } from 'ol/extent';
 import GeoTIFF from 'ol/source/GeoTIFF';
 import TileLayer from 'ol/layer/WebGLTile';
 import VectorLayer from 'ol/layer/Vector';
@@ -23,10 +24,15 @@ import 'ol/ol.css';
 import '../../styles/App.css';
 import { Polygon } from 'ol/geom';
 
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+
 const PlotMap = ({apiOutput}) => {
     // const navigate = useNavigate();
 
     const mapRef = useRef(null);
+    const [map, setMap] = useState(null);
+    const [vectorSource, setVectorSource] = useState(null);
     let gridDraw;
 
     useEffect(() => {
@@ -87,10 +93,12 @@ const PlotMap = ({apiOutput}) => {
             ],
             view: mapSource.getView(),        
         });
+        setMap(map)
+        setVectorSource(vectorSource)
+
         // map.addLayer(vectorLayer);
         const vectorExtent = vectorSource.getView();
         const mapViewExtent = map.getView();
-
 
         console.log(vectorExtent, mapViewExtent);
         //   console.log(map.getView().getProjection());
@@ -100,6 +108,77 @@ const PlotMap = ({apiOutput}) => {
           };
             
     }, [apiOutput]);
+
+    const exportPlotImages = async () => {
+      if (!vectorSource) return;
+      const features = vectorSource.getFeatures();
+      const zip = new JSZip();
+      console.log(vectorSource)
+
+      for (const feature of features) {
+          const extent = feature.getGeometry().getExtent();
+          const name = feature.get('name');
+          const imageBlob = await captureExtentAsImage(extent);
+          zip.file(`${name}.png`, imageBlob, { binary: true })
+      }
+
+      zip.generateAsync({type: 'blob'}).then((content) => {saveAs(content, "plot_images.zip")})
+
+  };
+
+    const captureExtentAsImage = async (extent) => {
+      return new Promise((resolve, reject) => {
+          if (!map) return reject(new Error('Map not initialized'));
+
+          const currentView = map.getView();
+          const currentCenter = currentView.getCenter();
+          const currentZoom = currentView.getZoom();
+
+          map.getView().fit(extent, { size: map.getSize() });
+
+          map.once('rendercomplete', () => {
+              const mapCanvas = map.getViewport().querySelector('canvas');
+
+              const bottomLeft = getBottomLeft(extent);
+              const topRight = getTopRight(extent);
+
+              const bottomLeftPixel = map.getPixelFromCoordinate(bottomLeft);
+              const topRightPixel = map.getPixelFromCoordinate(topRight);
+
+              // Calculate width and height of the area
+              const pixelRatio = window.devicePixelRatio || 1;
+              const width = Math.ceil((topRightPixel[0] - bottomLeftPixel[0]) * pixelRatio);
+              const height = Math.ceil((bottomLeftPixel[1] - topRightPixel[1]) * pixelRatio);
+
+              // Create a new canvas and context for the plot image
+              const canvas = document.createElement('canvas');
+              console.log("Pixel ratio: ", canvas.devicePixelRatio);
+              canvas.width = width;
+              canvas.height = height;
+              const context = canvas.getContext('2d');
+
+              if (mapCanvas) {
+                context.drawImage(
+                  mapCanvas,
+                  bottomLeftPixel[0] * pixelRatio, topRightPixel[1] * pixelRatio,
+                  width, height,
+                  0, 0,
+                  width, height
+              );
+
+              canvas.toBlob((blob) => {
+                  resolve(blob);
+                  map.getView().setCenter(currentCenter);
+                  map.getView().setZoom(currentZoom);
+              }, 'image/png');
+              } else {
+                  reject(new Error('Canvas element not found'));
+              }
+          });
+
+          map.renderSync();
+      });
+  };
 
     return (
       <Box
@@ -127,7 +206,7 @@ const PlotMap = ({apiOutput}) => {
 
                 </Grid>
         </Grid>
-        <Grid container spacing={2} 
+        <Grid container spacing={2}
         style={{
             backgroundColor: 'rgba(240,247,235,.5)',
             // position: 'relative',
@@ -138,7 +217,7 @@ const PlotMap = ({apiOutput}) => {
         mt={2}>
           <Grid item xs={12} sm={12} md={12} lg={12} id="map" ref={mapRef} style={{ width: '90%', height: '400px', padding: '10px'}} />
         </Grid>
-      </Box>
+        </Box>
     )
 };
 
